e28f4fe976e0137ea1f43104c889a7807d3aa4b7
ku13shov <kuleshov000@gmail.com>
2023-04-10 16:48:36 +0300

    Update 2023-04-10T13:48:34.013Z

--- app/js/main.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const cardsInner = document.querySelector('.cards__inner');
-
-cardsInner.addEventListener('click', (e) => {
-    if (e.target.closest('.card')) {
-        e.target.closest('.card').classList.toggle('active');
-    }
-});
